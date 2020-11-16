64b8d0e044e7c08c6254ef693b7df45f2d9c708e
Heber E. Senger <sengerhe@gmail.com>
2020-11-16 11:51:24 -0300

    testing info from server to connect mongo bg

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
 const rateLimit = require('express-rate-limit')
+const https = require('https')
+
+const port = process.env.PORT || 3000
+const secret = process.env.JWT_SECRET || 'place-production-secret-heroku-vars';
 
 // setup reqs limit to api
 const apiLimit = rateLimit({ windowMs: 60 * 1000, max: 6 })
@@ -11,8 +14,17 @@ app.use('/api/', apiLimit)
 // setup builded reactjs ui to be served from root
 app.use(express.static('ui/build'))
 
-app.get('/api/gettime', (req, res) => {
-  res.send('Server time: ' + new Date().toLocaleTimeString())
+app.get('/api/info', async (req, res) => {
+  https.get('https://api.ipify.org?format=json', ip => {
+    let body = '';
+    ip.on('data', d => body += d);
+    ip.on('end', () => {
+      const json = JSON.parse(body);
+      const content = `Server time: ${new Date()}<br/>`
+        + `Server IP: ${json.ip}`;
+      res.send(content);
+    });
+  })
 })
 
 app.listen(port, () => {

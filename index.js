259e949a7ea3e99793b17feb96a612ba127553c2
Heber E. Senger <sengerhe@gmail.com>
2020-11-16 20:58:00 -0300

    setup security jwt

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express = require('express')
 const app = express()
 const rateLimit = require('express-rate-limit')
 const https = require('https')
+const jwt = require('express-jwt')
+const token = require('jsonwebtoken')
+const MongoClient = require('mongodb').MongoClient
 
 const port = process.env.PORT || 3000
-const secret = process.env.JWT_SECRET || 'place-production-secret-heroku-vars';
+const secret = process.env.JWT_SECRET || 'place-production-secret-heroku-vars'
+const mongoUri = process.env.MONGO_URI
+
+const client = new MongoClient(mongoUri, { useNewUrlParser: true });
+client.connect(err => {
+  if (err) {
+    console.log(err)
+    return
+  }
+
+  client.close()
+})
 
 // setup reqs limit to api
-const apiLimit = rateLimit({ windowMs: 60 * 1000, max: 6 })
-app.use('/api/', apiLimit)
-
+app.use('/api/', rateLimit({ windowMs: 60 * 1000, max: 6 }))
+app.use('/login', rateLimit({ windowMs: 60 * 1000, max: 3 }))
 
 // setup builded reactjs ui to be served from root
 app.use(express.static('ui/build'))
@@ -23,9 +36,22 @@ app.get('/api/info', async (req, res) => {
       const content = `Server time: ${new Date()}<br/>`
         + `Server IP: ${json.ip}`;
       res.send(content);
-    });
+    })
   })
 })
+
+app.post('/login', (req, res) => {
+  const { uid, pwd } = req.body;
+  const content = token.sign({ uid }, secret)
+  res.json(content)
+})
+
+app.get('/protected',
+  jwt({ secret, algorithms: ['HS256'] }),
+  (req, res) => {
+    if (!req.user.admin) return res.sendStatus(401);
+    res.sendStatus(200);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

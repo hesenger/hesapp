852e6159f566427e4e188be1628b264e9dafeb5b
Heber E. Senger <sengerhe@gmail.com>
2020-11-16 10:20:46 -0300

    apply throttle to api reqs

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const rateLimit = require('express-rate-limit')
 
-app.use(express.static('app/build'))
+// setup reqs limit to api
+const apiLimit = rateLimit({ windowMs: 60 * 1000, max: 6 })
+app.use('/api/', apiLimit)
+
+
+// setup builded reactjs ui to be served from root
+app.use(express.static('ui/build'))
 
 app.get('/api/gettime', (req, res) => {
   res.send('Server time: ' + new Date().toLocaleTimeString())

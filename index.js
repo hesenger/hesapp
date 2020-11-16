80f96c787b26eb83f432c86c77b6865eda444693
Heber E. Senger <sengerhe@gmail.com>
2020-11-16 01:56:59 -0300

    configure reactjs in sub dir

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static('app/build'))
+
 app.get('/', (req, res) => {
   res.send('server time: ' + new Date().toLocaleTimeString())
 })
